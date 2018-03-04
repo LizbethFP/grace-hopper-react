67884f2bbbbd87a7c56ef5b9a5be53fa49c281db
LizbethFP <lizbeth.fp@gmail.com>
2018-03-04 17:14:28 -0500

    añadiendo clases a aside

--- src/components/aside.js
+++ src/components/aside.js
@@ -10,35 +10,35 @@ class Aside extends Component {
         <table className="summary-table">
           <thead>
             <tr>
-              <th colspan="2">Grace Murray Hopper</th>
+              <th colspan="2" className="text-center p-3">Grace Murray Hopper</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td colspan="2">
+              <td colspan="2" className="px-2">
                 <img src={graceHopper} alt="Grace Hopper Picture"></img>
-                  <p>Rear Admiral Grace M. Hopper, 1984</p>
+                  <p className="text-center mt-2">Rear Admiral Grace M. Hopper, 1984</p>
                   <hr></hr>
               </td>
             </tr>
-                <tr>
-                  <th>Nickname</th>
-                  <td>"Amazing Grace"</td>
-                </tr>
-                <tr>
-                  <th>Born</th>
-                  <td>December 9, 1906</td>
-                </tr>
-                <tr>
-                  <th>Died</th>
-                  <td>January 1, 1992 (aged 85)</td>
-                </tr>
-                <tr>
-                  <th>Allegiance</th>
-                  <td>
-                    <img src={usaFlag} alt="US flag"></img>
-                  </td>
-                </tr>
+            <tr>
+              <th className="pl-4 text-content">Nickname</th>
+              <td className="text-content">"Amazing Grace"</td>
+            </tr>
+            <tr>
+              <th className="pl-4 text-content">Born</th>
+              <td className="text-content">December 9, 1906</td>
+            </tr>
+            <tr>
+              <th className="pl-4 text-content">Died</th>
+              <td className="text-content">January 1, 1992 (aged 85)</td>
+            </tr>
+            <tr>
+              <th className="pl-4 text-content">Allegiance</th>
+              <td className="text-content">
+                <img src={usaFlag} alt="US flag"></img>
+              </td>
+            </tr>
 
         </tbody>
       </table>
