738e7c7bc3377dacb91f2db114b38f48d8d23126
LizbethFP <lizbeth.fp@gmail.com>
2018-03-04 17:25:20 -0500

    añadiendo estilo a main

--- src/components/main.js
+++ src/components/main.js
@@ -4,7 +4,7 @@ import './css/main.css';
 class Main extends Component {
   render() {
     return (
-      <main className="text-justify">
+      <main className="text-justify px-4">
 
         <p><strong>Grace Brewster Murray Hopper</strong> (née <strong>Murray</strong>; December 9, 1906 – January 1, 1992) was an American <a href="https://en.wikipedia.org/wiki/Computer_scientist" target="_blank">computer scientist</a> and <a href="https://en.wikipedia.org/wiki/United_States_Navy" target="_blank">United States</a> Navy rear admiral.[1] <mark>One of the first programmers of the <a href="https://en.wikipedia.org/wiki/Harvard_Mark_I" target="_blank">Harvard Mark I</a> computer</mark>, she was a pioneer of computer programming who invented one of the first compiler related tools. She popularized the idea of machine-independent programming languages, which led to the development of COBOL, an early high-level programming language still in use today.</p>
         <p>Hopper had attempted to enlist in the Navy during <a href="https://en.wikipedia.org/wiki/World_War_II" target="_blank">World War II</a>, but she was rejected by the military because she was 34 years of age and too old to enlist. She instead joined the Navy Reserves. Hopper began her computing career when she worked on the Harvard Mark I team that was led by Howard H. Aiken. In 1949, she joined the Eckert–Mauchly Computer Corporation and was part of the development team that designed the UNIVAC I computer in 1944. It was at Eckert–Mauchly that she began developing the compiler. She believed that computer code could be written in English by using a programming language that was based on English words. The compiler would convert that code into machine code that would be understood by computers. By 1952, Hopper finished her compiler, which was written for the A-0 System programming language.[2][3][4][5]</p>
@@ -12,8 +12,8 @@ class Main extends Component {
         <p>Owing to her accomplishments and her naval rank, she was sometimes referred to as "Amazing Grace".[6][7] The U.S. Navy Arleigh Burke-class guided-missile destroyer USS Hopper was named for her, as was the <a href="https://en.wikipedia.org/wiki/Cray_XE6" target="_blank">Cray XE6</a> "Hopper" supercomputer at NERSC.[8] During her lifetime, Hopper was awarded 40 honorary degrees from universities across the world. In 1991, she received the National Medal of Technology. On November 22, 2016, she was posthumously awarded the <a href="https://en.wikipedia.org/wiki/Presidential_Medal_of_Freedom" target="_blank">Presidential Medal of Freedom</a> by President <a href="https://en.wikipedia.org/wiki/Barack_Obama" target="_blank">Barack Obama</a>.[9]</p>
 
       
-      <h3>Career</h3>
-        <ol>
+      <h3 className="mt-4">Career</h3>
+        <ol className="mt-3">
           <li>World War II</li>
           <li>UNIVAC</li>
           <li>COBOL</li>
@@ -21,13 +21,13 @@ class Main extends Component {
         </ol>
 
       
-      <h2>Phrases</h2>
+      <h2 className="mt-4">Phrases</h2>
         <blockquote cite="http://www.azquotes.com/quote/553883">
           Humans are allergic to change. They love to say, 'We've always done it this way.' I try to fight that. That's why I have a clock on my wall that runs counter-clockwise.
       </blockquote>
 
       
-      <a href="information.html" target="_blank">See more</a>
+        <a href="https://lizbethfp.github.io/grace-hopper/information.html" target="_blank">See more</a>
 
       </main>
     );
